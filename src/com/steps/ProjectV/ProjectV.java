bf0b1df6ca108d6120abcca0c2cd0c3bb3ca54f1
Gio <giogogiashvili@yahoo.com>
2014-12-20 03:09:56 +0400

    added main layout alpha version

--- src/com/steps/ProjectV/ProjectV.java
+++ src/com/steps/ProjectV/ProjectV.java
@@ -1,4 +1,5 @@
 package com.steps.ProjectV;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
@@ -9,57 +10,52 @@ import android.provider.ContactsContract;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 import android.widget.AbsListView.LayoutParams;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.SimpleCursorAdapter;
+import com.example.ProjectV.R;
+import com.steps.Objects.GroupObject;
+import com.steps.adapters.GroupAdapter;
 
-public class ProjectV extends ListActivity{
+import java.util.ArrayList;
+
+public class ProjectV extends Activity {
     /**
      * Called when the activity is first created.
      */
 
-    // These are the Contacts rows that we will retrieve
-    static final String[] PROJECTION = new String[] {ContactsContract.Data._ID,
-            ContactsContract.Data.DISPLAY_NAME};
-
-    // This is the select criteria
-    static final String SELECTION = "((" +
-            ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
-            ContactsContract.Data.DISPLAY_NAME + " != '' ))";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main);
+        ListView listView = (ListView) findViewById(R.id.GroupListView);
         // Create a progress bar to display while the list loads
         ProgressBar progressBar = new ProgressBar(this);
         progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
         progressBar.setIndeterminate(true);
-        getListView().setEmptyView(progressBar);
+        listView.setEmptyView(progressBar);
 
         // Must add the progress bar to the root of the layout
         ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
         root.addView(progressBar);
 
-        // For the cursor adapter, specify which columns go into which views
-        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
-        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
-
-//        // Create an empty adapter we will use to display the loaded data.
-//        // We pass null for the cursor, then update it in onLoadFinished()
-//        mAdapter = new SimpleCursorAdapter(this,
-//                android.R.layout.simple_list_item_1, null,
-//                fromColumns, toViews, 0);
-//        setListAdapter(mAdapter);
+        // Construct the data source
+        ArrayList<GroupObject> arrayOfGroups= new ArrayList<GroupObject>();
+//        for(int i=0; i<100; i++) {
+//            arrayOfGroups.add(new GroupObject(1, "name", null));
+//        }
+//        // Create the adapter to convert the array to views
+//        GroupAdapter adapter = new GroupAdapter(this, arrayOfGroups);
+//        // Attach the adapter to a ListView
+//        listView.setAdapter(adapter);
 //
-//        // Prepare the loader.  Either re-connect with an existing one,
-//        // or start a new one.
-//        getLoaderManager().initLoader(0, null, this);
-////        setContentView(R.layout.main);
-    }
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // LinearLayout ani = (LinearLayout)view.findViewById(R.id.GroupListViewItemLinearLayout);
+//                // ani.startAnimation(new VisibleAni(1.0f, 1.0f, 0.0f, 1.0f, 800, ani, true,true));
+//                // ani.setVisibility(View.VISIBLE);
+//            }
+//        });
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        // Do something when a list item is clicked
     }
 }
--- src/com/steps/adapters/GroupAdapter.java
+++ src/com/steps/adapters/GroupAdapter.java
@@ -1,15 +1,43 @@
 package com.steps.adapters;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.ProjectV.R;
 import com.steps.Objects.GroupObject;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by Xato on 12/20/2014.
  */
 public class GroupAdapter extends ArrayAdapter<GroupObject> {
 
-    public GroupAdapter(Context context, int resource) {
-        super(context, resource);
+    public GroupAdapter(Context context, ArrayList<GroupObject> users) {
+        super(context, 0, users);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        GroupObject group = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.group, parent, false);
+        }
+        // Lookup view for data population
+        ImageView tvName = (ImageView) convertView.findViewById(R.id.GroupImageSmall);
+        TextView tvHome = (TextView) convertView.findViewById(R.id.GroupNameSmall);
+        // Populate the data into the template view using the data object
+        tvName.setImageDrawable(parent.getResources().getDrawable(R.drawable.ic_launcher));
+        tvHome.setText(group.getName());
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
