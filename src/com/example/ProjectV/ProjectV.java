e83a2dcb4075e596b4f8f094e53fa4df52e3ef82
Gio <giogogiashvili@yahoo.com>
2014-12-20 01:39:04 +0400

    started implementing listview

--- src/com/example/ProjectV/ProjectV.java
+++ src/com/example/ProjectV/ProjectV.java
@@ -1,15 +1,65 @@
 package com.example.ProjectV;
-
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView.LayoutParams;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
 
-public class ProjectV extends Activity {
+public class ProjectV extends ListActivity{
     /**
      * Called when the activity is first created.
      */
+
+    // These are the Contacts rows that we will retrieve
+    static final String[] PROJECTION = new String[] {ContactsContract.Data._ID,
+            ContactsContract.Data.DISPLAY_NAME};
+
+    // This is the select criteria
+    static final String SELECTION = "((" +
+            ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
+            ContactsContract.Data.DISPLAY_NAME + " != '' ))";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+        getListView().setEmptyView(progressBar);
+
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+        // For the cursor adapter, specify which columns go into which views
+        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
+        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
+
+//        // Create an empty adapter we will use to display the loaded data.
+//        // We pass null for the cursor, then update it in onLoadFinished()
+//        mAdapter = new SimpleCursorAdapter(this,
+//                android.R.layout.simple_list_item_1, null,
+//                fromColumns, toViews, 0);
+//        setListAdapter(mAdapter);
+//
+//        // Prepare the loader.  Either re-connect with an existing one,
+//        // or start a new one.
+//        getLoaderManager().initLoader(0, null, this);
+////        setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Do something when a list item is clicked
     }
 }
---
+++ src/com/steps/adapters/GroupAdapter.java
@@ -0,0 +1,10 @@
+package com.steps.adapters;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Xato on 12/20/2014.
+ */
+public class GroupAdapter extends ArrayAdapter<Group> {
+    
+}
