19fd0cea44c73671caf88a13e46eb4a825d5757d
Gio <giogogiashvili@yahoo.com>
2014-12-20 17:06:12 +0400

    added mainc construqtor

---
+++ src/org/steps/app/Activities/Constructor.java
@@ -0,0 +1,16 @@
+package org.steps.app.Activities;
+
+import android.content.ContentQueryMap;
+import android.content.Context;
+
+/**
+ * Created by Xato on 12/20/2014.
+ */
+public class Constructor {
+    public Constructor(Context context){
+        
+    }
+
+}
+
+
