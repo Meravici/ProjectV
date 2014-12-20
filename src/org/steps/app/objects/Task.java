44f16162674a89b1b5af4a1f9528b46053386673
Alex <alex.r.azizian@gmail.com>
2014-12-20 16:55:03 +0400

    added Objects

--- src/org/steps/app/objects/Task.java
+++ src/org/steps/app/objects/Task.java
@@ -1,7 +1,101 @@
 package org.steps.app.objects;
 
+import java.io.Serializable;
+import java.sql.Date;
+
 /**
  * Created by Xato on 12/20/2014.
  */
-public class Task {
+public class Task implements Serializable {
+    public static final int STATUS_PENDING = 0;
+    public static final int STATUS_STARTED = 1;
+    public static final int STATUS_FINISHED = 2;
+    public static final int STATUS_FAILED = 3;
+
+    private int id;
+    private String name;
+    private Date creationDate;
+    private Date dueDate;
+    private int status;
+    private User author;
+    private User volunteer;
+
+    public Task(int id, String name, Date creationDate, Date dueDate, int status, User author, User volunteer) {
+        this.id = id;
+        this.name = name;
+        this.creationDate = creationDate;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.author = author;
+        this.volunteer = volunteer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public User getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(User volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", creationDate=" + creationDate +
+                ", dueDate=" + dueDate +
+                ", status=" + status +
+                ", author=" + author +
+                ", volunteer=" + volunteer +
+                '}';
+    }
 }
