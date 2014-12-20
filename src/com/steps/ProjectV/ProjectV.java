d37d25f6ec24909dbdd163242b2c6da6e146ad05
Gio <giogogiashvili@yahoo.com>
2014-12-20 06:19:36 +0400

    added failure and success callbacks

--- src/com/steps/ProjectV/ProjectV.java
+++ src/com/steps/ProjectV/ProjectV.java
@@ -1,8 +1,10 @@
 package com.steps.ProjectV;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import com.example.ProjectV.R;
 import com.steps.Facade.FacadeAPI;
 import com.steps.Facade.FacadeMain;
 import com.steps.Objects.GroupObject;
+import com.steps.Objects.UserObject;
 import com.steps.Utils.Mediator;
 import com.steps.adapters.GroupAdapter;
 
@@ -28,13 +31,14 @@ public class ProjectV extends Activity {
      */
 
     private FacadeAPI facade;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
         this.facade = new FacadeMain(new Mediator(), this);
-        ListView listView = (ListView) findViewById(R.id.GroupListView);
+        listView = (ListView) findViewById(R.id.GroupListView);
         // Create a progress bar to display while the list loads
         ProgressBar progressBar = new ProgressBar(this);
         progressBar.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, Gravity.CENTER));
@@ -44,16 +48,6 @@ public class ProjectV extends Activity {
         // Must add the progress bar to the root of the layout
         ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
         root.addView(progressBar);
-
-        // Construct the data source
-        ArrayList<GroupObject> arrayOfGroups= new ArrayList<GroupObject>();
-        for(short i=0; i<100; i++) {
-            arrayOfGroups.add(new GroupObject(1, "სახელი", new Timestamp(1l), i)); //TODO
-        }
-        // Create the adapter to convert the array to views
-        GroupAdapter adapter = new GroupAdapter(this, arrayOfGroups);
-        // Attach the adapter to a ListView
-        listView.setAdapter(adapter);
 
 //        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
@@ -66,5 +60,34 @@ public class ProjectV extends Activity {
 //            }
 //        });
 
+    }
+
+    private void successCallback(UserObject loggedInUser, ArrayList<GroupObject> groups){
+        // Construct the data source
+        ArrayList<GroupObject> arrayOfGroups= new ArrayList<GroupObject>();
+        for(short i=0; i<100; i++) {
+            arrayOfGroups.add(new GroupObject(1, "სახელი", new Timestamp(1l), i)); //TODO
+        }
+        // Create the adapter to convert the array to views
+        GroupAdapter adapter = new GroupAdapter(this, arrayOfGroups);
+        // Attach the adapter to a ListView
+        listView.setAdapter(adapter);
+    }
+
+    private void failureCallback(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("CLOSE");
+        builder.setMessage("Do You Want to Close the Application").setCancelable(false)
+                .setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                                finish();
+                            }
+
+
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
