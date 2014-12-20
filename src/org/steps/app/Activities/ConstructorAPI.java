ab9edfe71fe78adef31762685bcd29c9159e1afc
Alex <alex.r.azizian@gmail.com>
2014-12-20 20:58:43 +0400

    added ConstructorAPI

---
+++ src/org/steps/app/Activities/ConstructorAPI.java
@@ -0,0 +1,14 @@
+package org.steps.app.Activities;
+
+import org.steps.app.objects.Group;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 12/20/2014.
+ */
+public interface ConstructorAPI {
+    public void getGroups();
+    public void getGroupsCallback(ArrayList<Group> groups);
+    public void login(String s);
+}
